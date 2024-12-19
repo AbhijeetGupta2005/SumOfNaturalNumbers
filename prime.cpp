568d26bc42cff4fdf7345c3e43ec65b98d9e33c7
AbhijeetGupta2005 <149326773+AbhijeetGupta2005@users.noreply.github.com>
2024-12-19 11:40:31 +0530

    Update prime.cpp

--- prime.cpp
+++ prime.cpp
@@ -1,33 +1,23 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
 bool isPrime(int n) {
-    if (n <= 1) {
-        return false;
-    }
-    if (n == 2) {
-        return true;
-    }
-    if (n % 2 == 0) {
-        return false;
-    }
-    for (int i = 3; i * i <= n; i += 2) {
-        if (n % i == 0) {
-            return false;
-        }
+    if (n <= 1) return false;
+    for (int i = 2; i * i <= n; ++i) {
+        if (n % i == 0) return false;
     }
     return true;
 }
 
 int main() {
-    int num;
+    int number;
     cout << "Enter a number: ";
-    cin >> num;
+    cin >> number;
 
-    if (isPrime(num)) {
-        cout << num << " is a prime number." << endl;
+    if (isPrime(number)) {
+        cout << number << " is a prime number." << endl;
     } else {
-        cout << num << " is not a prime number." << endl;
+        cout << number << " is not a prime number." << endl;
     }
 
     return 0;
