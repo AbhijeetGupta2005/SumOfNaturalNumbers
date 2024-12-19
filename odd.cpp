c5a6659fdb600871def9cf22e79fd29fd94a9037
AbhijeetGupta2005 <149326773+AbhijeetGupta2005@users.noreply.github.com>
2024-12-19 12:00:14 +0530

    Update odd.cpp

--- odd.cpp
+++ odd.cpp
@@ -1,13 +1,18 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+int main(int argc, char* argv[]) {
   int n;
-  cout << "Odd Number: ";
-  cin >> n;
-  for(int i=1; i<n; i++){
-    if(i%2!=0){
-      cout<<i<<" ";
+  if (argc > 1) {
+    n = atoi(argv[1]);
+  } else {
+    cout << "Odd Number: ";
+    cin >> n;
+  }
+
+  for(int i = 1; i < n; i++) {
+    if(i % 2 != 0) {
+      cout << i << " ";
     }
   }
   return 0;
