2b7f36aee00ee518f80bd3ac3111990a4a42e93d
AbhijeetGupta2005 <149326773+AbhijeetGupta2005@users.noreply.github.com>
2024-12-19 11:14:09 +0530

    Update sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
 using namespace std;
+
 int main(){
-  int n;
-  cout<<"Natural Number: ";
-  cin>>n;
-  int sum=n*(n+1)/2;
-  cout<<"Sum of first "<<n<<"natural numbers is: "<<sum;
+  long long n;
+  cout << "Natural Number: ";
+  cin >> n;
+  long long sum = n * (n + 1) / 2;
+  cout << "Sum of first " << n << " natural numbers is: " << sum << endl;
   return 0;
 }
